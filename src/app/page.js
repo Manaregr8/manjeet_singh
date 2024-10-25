9a44ee095d63af10030967d2472fca42c66b2944
Manjeet Singh <99804338+Manaregr8@users.noreply.github.com>
2024-10-25 10:06:46 +0000

    making global styles

--- src/app/page.js
+++ src/app/page.js
@@ -9,6 +9,31 @@ export default function Home() {
           height={38}
           priority
         />
+        <h1>Manjeet Singh</h1>
+        <h2>I am a developer</h2>
+        <h3>holla</h3>
+        <button>
+          Submit
+        </button>
+        <a className="links">
+        <Image
+        className="linksImage"
+          src="/window.svg"
+          alt="linkdin Logo"
+          width={20}
+          height={20}
+          priority
+        />
+        <Image
+        className="linksImage"
+          src="/vercel.svg"
+          alt="Profile Image"
+          width={20}
+          height={20}
+          priority
+        />
+        <p className="linksName">codepen</p>
+        </a>
       </main>
   );
 }
