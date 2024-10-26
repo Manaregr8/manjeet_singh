7898777c13f88b2e971fcb6cd8b4f99126e8d1aa
Manjeet Singh <99804338+Manaregr8@users.noreply.github.com>
2024-10-26 12:20:09 +0000

    footer and global styles

---
+++ src/app/components/footer.js
@@ -0,0 +1,10 @@
+export default function Footer(){
+   return (
+<footer style={{position:'fixed',bottom:'0',width:'100%', left:'0'}}>
+   <div className="codesTitle" style={{border:'none', borderRadius:'0px', padding:'20px'}}> 
+   <p className="mono-font">@copywright 2024 By Manjeet Singh</p>
+   <a className="margin-left" href="tel:8506040783">8506040783</a>
+   </div>
+   </footer>
+   );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Footer from "./components/footer";
 export default function Home() {
   return (
       <main>  
@@ -29,27 +30,17 @@ export default function Home() {
         />
         <p className="linksName">codepen</p>
         </a>
-        <a className="links">
-        <Image
-        className="linksImage"
-          src="/codepen.png"
-          alt="linkdin Logo"
-          width={30}
-          height={30}
-          priority
-        />
-        <p>-</p>
-        <Image
-        className="linksImage"
-          src="/codepenProfile.jpeg"
-          alt="Profile Image"
-          width={30}
-          height={30}
-          priority
-        />
-        <p className="linksName">codepen</p>
-        </a>
         </div>
+        <div className="codesContainer">
+          <div className="codesTitle">
+            <p className="mono-font">Title</p>
+            <p className="mono-font margin-left copy">copy</p>
+          </div>
+          <div className="codes">
+          <p className="mono-font">const newVariable = document.getElementById("root");</p>
+          </div>
+        </div>
+        <Footer />
       </main>
   );
 }
