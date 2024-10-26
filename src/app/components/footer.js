6d42b4a641af1bc52eae9bf4dd749bd080f6adf6
Manjeet Singh <99804338+Manaregr8@users.noreply.github.com>
2024-10-26 12:33:10 +0000

    changed contact number

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -3,7 +3,7 @@ export default function Footer(){
 <footer style={{position:'fixed',bottom:'0',width:'100%', left:'0'}}>
    <div className="codesTitle" style={{border:'none', borderRadius:'0px', padding:'20px'}}> 
    <p className="mono-font">@copywright 2024 By Manjeet Singh</p>
-   <a className="margin-left" href="tel:8506040783">8506040783</a>
+   <a className="margin-left" href="tel:+918506040783">+91-8506040783</a>
    </div>
    </footer>
    );
