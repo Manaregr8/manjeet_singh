5dae473d8f46dde2a34cd31a6a1ba863856a9b3e
Manjeet Singh <99804338+Manaregr8@users.noreply.github.com>
2024-10-27 03:53:37 +0000

    banner creation

---
+++ src/app/components/mainBanner.js
@@ -0,0 +1,13 @@
+import Styles from "../styles/banner.css";
+
+export default function Banner() {
+    return (
+        <div className="bannerContainer">
+            <div className="bannerCover">
+                <main>
+            <h1>Build Your Brand With Us</h1>
+            </main>
+            </div>
+        </div>
+    );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Extra from "./components/extra.js";
+import MainBanner from "./components/mainBanner.js";
 
 export default function Home() {
   return (
-      <main>  
+      <>  
+        <MainBanner />
         <Extra />
-      </main>
+      </>
   );
 }
