a093ff04abd0361304e92e3ab1ffe39af72193c3
Manjeet Singh <99804338+Manaregr8@users.noreply.github.com>
2024-10-27 17:13:41 +0000

    loader styled in css

--- src/app/components/loader.js
+++ src/app/components/loader.js
@@ -5,13 +5,9 @@ export default function Loader() {
     const opening = `<!--Hi There! Welcome to Manjeet's Website--!>`;
     return (
         <div className='loader'>
-            <Image className="loaderimage"
-            src="/loader.gif"
-            alt="loader"
-            width={80}
-            height={80}
-            priority
-            />
+            <div className="loadingio-spinner-ripple-nq4q5u6dq7r"><div className="ldio-x2uulkbinbj">
+                <div></div><div></div>
+            </div></div>
             <p className="typewriter-text mono-font green">{opening}</p>
         </div>
     );
