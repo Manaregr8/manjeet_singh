8c730eb9280e42fe816cc0f125edfcb4ef018cb2
Manjeet Singh <99804338+Manaregr8@users.noreply.github.com>
2024-10-29 17:21:55 +0000

    loader timeout function added

--- src/app/components/loader.js
+++ src/app/components/loader.js
@@ -1,14 +1,31 @@
+"use client";
 import Styles from "../styles/loader.css";
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Loader() {
-    const opening = `<!--Hi There! Welcome to Manjeet's Website--!>`;
-    return (
-        <div className='loader'>
-            <div className="loadingio-spinner-ripple-nq4q5u6dq7r"><div className="ldio-x2uulkbinbj">
-                <div></div><div></div>
-            </div></div>
-            <p className="typewriter-text mono-font green">{opening}</p>
+  const opening = `<!--Hi There! Welcome to Manjeet's Website-->`;
+
+  useEffect(() => {
+    const hideLoader = () => {
+      setTimeout(() => {
+        const loaderElement = document.querySelector(".loader");
+        if (loaderElement) {
+          loaderElement.style.display = 'none';
+        }
+      }, 5500);
+    };
+    hideLoader();
+  }, []);
+
+  return (
+    <div className="loader">
+      <div className="loadingio-spinner-ripple-nq4q5u6dq7r">
+        <div className="ldio-x2uulkbinbj">
+          <div></div><div></div>
         </div>
-    );
+      </div>
+      <p className="typewriter-text mono-font green">{opening}</p>
+    </div>
+  );
 }
