82fdd757ca95b3031402a889fc5052a8d9cfd022
Manjeet Singh <99804338+Manaregr8@users.noreply.github.com>
2024-11-02 09:45:25 +0000

    fixed errors

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect } from "react";
 import Styles from "../styles/navbar.css";
-
+import Link from "next/link";
 export default function Navbar(){
     useEffect(() => {
         const menuToggler = document.getElementsByClassName("menuToggler")[0];
@@ -24,10 +24,10 @@ export default function Navbar(){
         <nav>
             <h5>MANJEET SINGH</h5>
             <div className="navlinks">
-                <a href="/">HOME</a>
-                <a href="/about">ABOUT</a>
-                <a href="/projects">PROJECTS</a>
-                <a href="/Hire us">HIRE US</a>
+                <Link href="/">HOME</Link>
+                <Link href="/about">ABOUT</Link>
+                <Link href="/projects">PROJECTS</Link>
+                <Link href="/Hire us">HIRE US</Link>
             </div>
             <button className="menuToggler">MENU</button>
             <button className="hireMe">HIRE ME</button>
