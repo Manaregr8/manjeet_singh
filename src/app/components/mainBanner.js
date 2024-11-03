de3eb49c7d4fab476dbe28358820c0dd8f9a2d8d
Manjeet Singh <99804338+Manaregr8@users.noreply.github.com>
2024-11-03 15:11:21 +0000

    banner addition

--- src/app/components/mainBanner.js
+++ src/app/components/mainBanner.js
@@ -3,7 +3,14 @@ import Navbar from "./navbar.js";
 export default function Banner() {
     return (
         <div className="bannerContainer">
-            <h1>MANJEET SINGH</h1>
+            <div className="bannerSize">
+                <div className="firstContainer">
+                    <h1>CREATIVE</h1>
+                </div>
+                <div className="secondContainer">
+                    <h1>DEVELOPER</h1>
+                </div>
+            </div>
         </div>
     );
 }
