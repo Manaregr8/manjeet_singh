b4cf5c5272d7e1f85a17b279c147ab8975c1c812
Manjeet Singh <99804338+Manaregr8@users.noreply.github.com>
2024-11-10 06:12:47 +0000

    banner ui updation

--- src/app/components/mainBanner.js
+++ src/app/components/mainBanner.js
@@ -1,13 +1,52 @@
 import Styles from "../styles/banner.css";
 import Navbar from "./navbar.js";
 export default function Banner() {
+    const content1 = (
+        <>
+            <span className="codeBlueDark">document</span>
+            <span className="codePurple">.getElementById</span>
+            <span className="codeYellow">(</span>
+            <span className="codeBlue">"manjeet"</span>
+            <span className="codeYellow">)</span> <br />
+            <span className="codePurple">.addEventListener</span>
+            <span className="codeYellow">(</span>
+            <span className="codeBlue"> "click" </span>,
+            <span className="codeBlueDark"> console</span> 
+            <span className="codePurple">.log</span><br />
+            <span className="codeGreen">(</span>
+            <span className="codeBlue">"awww you like meeeee?!!!"</span>
+            <span className="codeGreen">)</span>
+            <span className="codeYellow">)</span>;
+        </>
+    );
+    const content2= (
+        <>
+            <span className="codeBlueDark">document</span>
+            <span className="codePurple">.getElementById</span>
+            <span className="codeYellow">(</span>
+            <span className="codeBlue">"food"</span>
+            <span className="codeYellow">)</span> <br />
+            <span className="codePurple">.addEventListener</span>
+            <span className="codeYellow">(</span>
+            <span className="codeBlue"> "click" </span>,
+            <span className="codeBlueDark"> console</span> 
+            <span className="codePurple">.log</span><br />
+            <span className="codeGreen">(</span>
+            <span className="codeBlue">"No Coffee Only KFC"</span>
+            <span className="codeGreen">)</span>
+            <span className="codeYellow">)</span>;
+        </>
+    );
     return (
         <div className="bannerContainer">
             <div className="bannerSize">
                 <div className="firstContainer">
                     <h1>CREATIVE</h1>
+                    <div className="graphic1"><p className="mono-font">{content1}</p>
+                    </div>
                 </div>
                 <div className="secondContainer">
+                <div className="graphic2"><p className="mono-font">{content2}</p></div>
                     <h1>DEVELOPER</h1>
                 </div>
             </div>
