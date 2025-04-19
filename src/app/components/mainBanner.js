0e7b375ac2d749268af481f31a53ee383eab6370
Manjeet Singh <99804338+Manaregr8@users.noreply.github.com>
2025-04-19 07:23:09 +0000

    changed ui for errors

--- src/app/components/mainBanner.js
+++ src/app/components/mainBanner.js
@@ -2,41 +2,43 @@ import Styles from "../styles/banner.css";
 import Navbar from "./navbar.js";
 export default function Banner() {
     const content1 = (
-        <>
-            <span className="codeBlueDark">document</span>
-            <span className="codePurple">.getElementById</span>
-            <span className="codeYellow">(</span>
-            <span className="codeBlue">"manjeet"</span>
-            <span className="codeYellow">)</span> <br />
-            <span className="codePurple">.addEventListener</span>
-            <span className="codeYellow">(</span>
-            <span className="codeBlue"> "click" </span>,
-            <span className="codeBlueDark"> console</span> 
-            <span className="codePurple">.log</span><br />
-            <span className="codeGreen">(</span>
-            <span className="codeBlue">"awww you like meeeee?!!!"</span>
-            <span className="codeGreen">)</span>
-            <span className="codeYellow">)</span>;
-        </>
-    );
-    const content2= (
-        <>
-            <span className="codeBlueDark">document</span>
-            <span className="codePurple">.getElementById</span>
-            <span className="codeYellow">(</span>
-            <span className="codeBlue">"food"</span>
-            <span className="codeYellow">)</span> <br />
-            <span className="codePurple">.addEventListener</span>
-            <span className="codeYellow">(</span>
-            <span className="codeBlue"> "click" </span>,
-            <span className="codeBlueDark"> console</span> 
-            <span className="codePurple">.log</span><br />
-            <span className="codeGreen">(</span>
-            <span className="codeBlue">"No Coffee Only KFC"</span>
-            <span className="codeGreen">)</span>
-            <span className="codeYellow">)</span>;
-        </>
-    );
+  <>
+    <span className="codeBlueDark">document</span>
+    <span className="codePurple">.getElementById</span>
+    <span className="codeYellow">(</span>
+    <span className="codeBlue">&quot;manjeet&quot;</span>
+    <span className="codeYellow">)</span> <br />
+    <span className="codePurple">.addEventListener</span>
+    <span className="codeYellow">(</span>
+    <span className="codeBlue">&quot;click&quot;</span>,{" "}<br />
+    <span className="codeBlueDark">console</span>
+    <span className="codePurple">.log</span>
+    <span className="codeGreen">(</span>
+    <span className="codeBlue">&quot;awww you like meeeee?!!!&quot;</span>
+    <span className="codeGreen">)</span>
+    <span className="codeYellow">)</span>;
+  </>
+);
+
+const content2 = (
+  <>
+    <span className="codeBlueDark">document</span>
+    <span className="codePurple">.getElementById</span>
+    <span className="codeYellow">(</span>
+    <span className="codeBlue">&quot;food&quot;</span>
+    <span className="codeYellow">)</span> <br />
+    <span className="codePurple">.addEventListener</span>
+    <span className="codeYellow">(</span>
+    <span className="codeBlue">&quot;click&quot;</span>,{" "}<br />
+    <span className="codeBlueDark">console</span>
+    <span className="codePurple">.log</span>
+    <span className="codeGreen">(</span>
+    <span className="codeBlue">&quot;No Coffee Only KFC&quot;</span>
+    <span className="codeGreen">)</span>
+    <span className="codeYellow">)</span>;
+  </>
+);
+
     return (
         <div className="bannerContainer">
             <div className="bannerSize">
